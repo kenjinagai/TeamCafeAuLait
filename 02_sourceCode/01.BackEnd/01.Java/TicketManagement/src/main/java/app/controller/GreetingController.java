fcb18b05e0c57b9d4c796af2ea92ed876129c32e
KenjiNagai <s07430@gmail.com>
2017-04-15 18:21:20 +0900

    Fix using HTTP Status

--- 02_sourceCode/01.BackEnd/01.Java/TicketManagement/src/main/java/app/controller/GreetingController.java
+++ 02_sourceCode/01.BackEnd/01.Java/TicketManagement/src/main/java/app/controller/GreetingController.java
@@ -3,6 +3,8 @@ package app.controller;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,7 +20,7 @@ public class GreetingController {
 	GreetingService greetingService;
 
 	@RequestMapping("/greeting")
-	public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name){
-		return greetingService.getGreet(counter, name);
+	public ResponseEntity<Greeting> greeting(@RequestParam(value="name", defaultValue="World") String name){
+		return new ResponseEntity<Greeting>(greetingService.getGreet(counter, name), HttpStatus.OK);
 	}
 }
