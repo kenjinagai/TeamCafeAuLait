7dccf7bd4a3f640cad4df82ca5db2ace63d5ecde
kenji.nagai <s07430@gmail.com>
2017-04-22 11:04:40 +0900

    Delete DataBase Access. Idm

--- 02_sourceCode/01.BackEnd/01.Java/IdentityManagement/src/main/java/app/Application.java
+++ 02_sourceCode/01.BackEnd/01.Java/IdentityManagement/src/main/java/app/Application.java
@@ -4,7 +4,6 @@ package app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
@@ -12,9 +11,4 @@ public class Application extends SpringBootServletInitializer {
 	public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
 	}
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Application.class);
-    }
 }
