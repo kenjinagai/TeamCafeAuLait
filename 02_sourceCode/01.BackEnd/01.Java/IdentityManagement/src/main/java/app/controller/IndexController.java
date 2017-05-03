4a37d26b2a606cf25644a6725a4a4d2727d9e059
kenji.nagai <s07430@gmail.com>
2017-05-03 13:20:09 +0900

    Update enviroment. Add class diagram.

---
+++ 02_sourceCode/01.BackEnd/01.Java/IdentityManagement/src/main/java/app/controller/IndexController.java
@@ -0,0 +1,15 @@
+package app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class IndexController {
+
+	@RequestMapping(value="/index",method=RequestMethod.GET)
+	public String indexUrl(){
+		return "index.html";
+	}
+
+}
