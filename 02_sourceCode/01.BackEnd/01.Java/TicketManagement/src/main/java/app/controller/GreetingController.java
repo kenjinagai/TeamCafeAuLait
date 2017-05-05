14ee04c4b1983ab733398dd4ff0141783099e337
kenji.nagai <s07430@gmail.com>
2017-05-05 19:58:06 +0900

    Polish swagger. GreetingController

--- 02_sourceCode/01.BackEnd/01.Java/TicketManagement/src/main/java/app/controller/GreetingController.java
+++ 02_sourceCode/01.BackEnd/01.Java/TicketManagement/src/main/java/app/controller/GreetingController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,12 +29,12 @@ public class GreetingController {
 	@Autowired
 	TestDbService sqlService;
 
-	@RequestMapping("/greeting")
+	@RequestMapping(value="/greeting", method=RequestMethod.GET)
 	public ResponseEntity<Greeting> greeting(@RequestParam(value="name", defaultValue="World") String name){
 		return new ResponseEntity<Greeting>(greetingService.getGreet(counter, name), HttpStatus.OK);
 	}
 
-	@RequestMapping("/select_test")
+	@RequestMapping(value="/select_test", method=RequestMethod.GET)
 	public ResponseEntity<List<Map<String, Object>>> selectTest(@RequestParam(value="name", defaultValue="World") String name){
 		return new ResponseEntity<List<Map<String, Object>>>(sqlService.home(), HttpStatus.OK);
 	}
