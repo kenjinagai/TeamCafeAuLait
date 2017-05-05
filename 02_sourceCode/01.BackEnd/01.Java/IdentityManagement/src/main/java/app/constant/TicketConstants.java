e83ed6d52bc83f745e59ba1a33b10482513fd89d
kenji.nagai <s07430@gmail.com>
2017-05-05 19:11:31 +0900

    Add authrization. IdentityManagement

--- 02_sourceCode/01.BackEnd/01.Java/IdentityManagement/src/main/java/app/constant/TicketConstants.java
+++ 02_sourceCode/01.BackEnd/01.Java/IdentityManagement/src/main/java/app/constant/TicketConstants.java
@@ -3,4 +3,5 @@ package app.constant;
 public interface TicketConstants {
 	public static final String TEMPLATE = "Hello, %s";
 	public static final String XML_MYBATIS_SETTING = "mybatis-config.xml";
+	public static final String ROLE_ADMIN = "ROLE_ADMIN";
 }
--- 02_sourceCode/01.BackEnd/01.Java/IdentityManagement/src/main/java/app/controller/UserController.java
+++ 02_sourceCode/01.BackEnd/01.Java/IdentityManagement/src/main/java/app/controller/UserController.java
@@ -2,13 +2,19 @@ package app.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import app.entity.User;
 import app.repository.UserRepository;
+import app.service.AuthrizationService;
 
 /**
  * デモ用データ取得用コントローラ
@@ -21,8 +27,14 @@ public class UserController {
 	@Autowired
 	private UserRepository repository;
 
-	@RequestMapping(value="/api/v1/users",method=RequestMethod.GET)
-	public List<User> findAll(){
-		return repository.findAll();
+	@Autowired
+	private AuthrizationService authrization;
+
+	@RequestMapping(value="/users",method=RequestMethod.GET)
+	public ResponseEntity<List<User>> findAll(@RequestHeader(value = "X-XSRF-TOKEN") String token, HttpServletRequest request){
+		if(!authrization.isAdmin(request)){
+			return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+		}
+		return new ResponseEntity<List<User>>(repository.findAll(),HttpStatus.OK);
 	}
 }
