771ba78ac2f8bd6a5a14cb5f4a1837bbba59d5c5
kenji.nagai <s07430@gmail.com>
2017-05-05 19:12:46 +0900

    Add authrization. IdentityManagement

---
+++ 02_sourceCode/01.BackEnd/01.Java/IdentityManagement/src/main/java/app/service/AuthrizationService.java
@@ -0,0 +1,18 @@
+package app.service;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import app.constant.TicketConstants;
+
+@Service
+public class AuthrizationService {
+	private static final Logger logger = LoggerFactory.getLogger(AuthrizationService.class);
+
+	public boolean isAdmin(HttpServletRequest request){
+		return request.isUserInRole(TicketConstants.ROLE_ADMIN);
+	}
+}
