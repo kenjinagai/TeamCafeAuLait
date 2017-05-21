7227f17f6b0e064348c23802c460e6c0a9241a8c
kenji.nagai <s07430@gmail.com>
2017-05-21 16:49:04 +0900

    Add constants.

--- 02_sourceCode/01.BackEnd/01.Java/IdentityManagement/src/main/java/app/constant/TicketConstants.java
+++ 02_sourceCode/01.BackEnd/01.Java/IdentityManagement/src/main/java/app/constant/TicketConstants.java
@@ -4,4 +4,6 @@ public interface TicketConstants {
     public static final String TEMPLATE = "Hello, %s";
     public static final String XML_MYBATIS_SETTING = "mybatis-config.xml";
     public static final String ROLE_ADMIN = "ROLE_ADMIN";
+    public static final int MAX_TEXT_LENGHT = 15;
+    public static final String COOKIE_NAME_CSRF = "XSRF-TOKEN";
 }
