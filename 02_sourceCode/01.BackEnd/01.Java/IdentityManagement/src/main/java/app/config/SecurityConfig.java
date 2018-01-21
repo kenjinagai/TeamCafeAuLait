61f698c428c3065500ae9e5e664c49017ad144a4
kenji.nagai <s07430@gmail.com>
2018-01-21 10:58:50 +0900

    [Delete] unused config class. [Polish] code

--- 02_sourceCode/01.BackEnd/01.Java/IdentityManagement/src/main/java/app/config/SecurityConfig.java
+++ 02_sourceCode/01.BackEnd/01.Java/IdentityManagement/src/main/java/app/config/SecurityConfig.java
@@ -125,19 +125,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public CorsConfigurationSource corsConfigurationSource() {
         final CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(ImmutableList.of("*"));
-        configuration.setAllowedMethods(ImmutableList.of("HEAD",
-                "GET", "POST", "PUT", "DELETE", "PATCH"));
+        configuration.setAllowedMethods(
+                ImmutableList.of("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH"));
         // setAllowCredentials(true) is important, otherwise:
         // The value of the 'Access-Control-Allow-Origin' header in the response must not be the wildcard '*' when the request's credentials mode is 'include'.
         configuration.setAllowCredentials(true);
         // setAllowedHeaders is important! Without it, OPTIONS preflight request
-        // will fail with 403 Invalid CORS request
-        //        configuration.setAllowedHeaders(
-        //                ImmutableList.of("Access-Control-Allow-Origin", "Origin", "Authorization",
-        //                        "Cache-Control", "Content-Type",
-        //                        "X-XSRF-TOKEN"));
-        configuration.setAllowedHeaders(
-                ImmutableList.of("*"));
+        configuration.setAllowedHeaders(ImmutableList.of("*"));
 
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
